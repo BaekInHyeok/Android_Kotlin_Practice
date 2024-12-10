523eca24342ba6eae887fc4659d590182f2d89a3
BaekInHyeok <bih63879@gmail.com>
2024-12-10 23:19:45 +0900

    코틀린 자습 1 : 정수와 상수 개념, 기본 자료형

---
+++ Kotlin_Practice_1/app/src/main/java/com/example/kotlin_practice_1/main.kt
@@ -0,0 +1,30 @@
+fun main(args:Array<String>){
+    //정수와 상수
+    val pi : Double =3.14//상수 : 값을 재할당할 수 없음
+    var age : Int = 28//변수 : 값을 재할당할 수 있음
+    println(pi)
+    println(age)
+    println("Hello word")
+
+    //[Data Type]
+
+    //1.Numbers
+    //1-1.정수형
+    var mByte : Byte = 100
+    var mShort : Short = 1000
+    var mInt : Int = 10000
+    var mLong : Long = 10000000
+    //1-2.실수형
+    var mFloat : Float = 1.5f
+    var mDouble : Double = 1.439282
+    //2.Characters
+    var mChar : Char='b'
+    //3.String
+    var mString : String = "Hello World"
+    //4.Boolean
+    var mtrue : Boolean = true
+    var mfalse : Boolean = false
+    //5.Arrays
+    var mArray : Array<String> = arrayOf("Hello","World")
+
+}
