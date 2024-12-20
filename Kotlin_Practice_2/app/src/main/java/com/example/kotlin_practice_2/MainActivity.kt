c0f987d32d69b321a3c946c0650304c8ab3dba03
BaekInHyeok <bih63879@gmail.com>
2024-12-20 13:34:37 +0900

    코틀린 연습 : 액티비티간 이동

--- Kotlin_Practice_2/app/src/main/java/com/example/kotlin_practice_2/MainActivity.kt
+++ Kotlin_Practice_2/app/src/main/java/com/example/kotlin_practice_2/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.kotlin_practice_2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -8,6 +10,14 @@ class MainActivity : AppCompatActivity() {//MainActivity -> AppCompatActivity ->
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
+    //버튼 누르면 SubActivity로 이동
+    fun settingButton(){
+        val button = findViewById<Button>(R.id.button);
+        button.setOnClickListener{
+            val intent = Intent(this, SubActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ Kotlin_Practice_2/app/src/main/java/com/example/kotlin_practice_2/SubActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin_practice_2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SubActivity : AppCompatActivity() {//MainActivity -> AppCompatActivity -> Activity -> Context
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_sub)
+
+}
+}
